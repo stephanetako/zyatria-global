905195c511998e716f5d1d72584692df45a368f9
stephanechevry-dev <stephanechevry@gmail.com>
2026-05-23 18:10:49 -0400

    Clean repository - Ready for deployment

---
+++ src/config/stripe-links.ts
@@ -0,0 +1,16 @@
+﻿export const STRIPE_LINKS = {
+  audit: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk',
+  starter: {
+    setup: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk',
+    monthly: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk'
+  },
+  professional: {
+    setup: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk',
+    monthly: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk'
+  },
+  enterprise: {
+    setup: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk',
+    monthly: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk'
+  },
+  consultation: 'https://buy.stripe.com/test_28o5lq0Hy0Hy2Ry5kk'
+};
