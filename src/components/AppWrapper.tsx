9a2581f838fd4f578c7f515afc94b46eed7c5d9d
stephanechevry-dev <stephanechevry@gmail.com>
2026-08-27 22:44:24 -0400

    Add AppWrapper component

---
+++ src/components/AppWrapper.tsx
@@ -0,0 +1,42 @@
+﻿import React from 'react';
+import { LanguageProvider } from '../lib/language-context';
+import NavigationDesignSystem from './NavigationDesignSystem';
+import HeroDesignSystem from './HeroDesignSystem';
+import TrustStatsSimple from './TrustStatsSimple';
+import Services from './Services';
+import MicroAgents from './MicroAgents';
+import RoadmapDesignSystem from './RoadmapDesignSystem';
+import PricingDesignSystem from './PricingDesignSystem';
+import TestimonialsDesignSystem from './TestimonialsDesignSystem';
+import FAQDesignSystem from './FAQDesignSystem';
+import CTAFinal from './CTAFinal';
+import FooterDesignSystem from './FooterDesignSystem';
+import IntelligentChatBot from './IntelligentChatBot';
+import CookieConsent from './CookieConsent';
+
+const AppWrapper: React.FC = () => {
+  return (
+    <LanguageProvider>
+      <div className="min-h-screen bg-background text-foreground">
+        <NavigationDesignSystem />
+        <main>
+          <HeroDesignSystem />
+          <TrustStatsSimple />
+          <Services />
+          <MicroAgents />
+          <RoadmapDesignSystem />
+          <PricingDesignSystem />
+          <TestimonialsDesignSystem />
+          <FAQDesignSystem />
+          <CTAFinal />
+        </main>
+        <FooterDesignSystem />
+        {/* Chatbot intelligent autonome */}
+        <IntelligentChatBot />
+        <CookieConsent />
+      </div>
+    </LanguageProvider>
+  );
+};
+
+export default AppWrapper;
