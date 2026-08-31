a6bdbf258c2bb2f845b3102de549cb4a9d94e030
stephanechevry-dev <stephanechevry@gmail.com>
2026-08-30 23:24:13 -0400

    Restore complete AppWrapper with all sections

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,11 +1,41 @@
 ﻿import React from 'react';
+import { LanguageProvider } from '../lib/language-context';
+import NavigationDesignSystem from './NavigationDesignSystem';
+import HeroDesignSystem from './HeroDesignSystem';
+import TrustStatsSimple from './TrustStatsSimple';
+import Services from './Services';
+import MicroAgents from './MicroAgents';
+import RoadmapDesignSystem from './RoadmapDesignSystem';
+import PricingDesignSystem from './PricingDesignSystem';
+import TestimonialsDesignSystem from './TestimonialsDesignSystem';
+import FAQDesignSystem from './FAQDesignSystem';
+import CTAFinal from './CTAFinal';
+import FooterDesignSystem from './FooterDesignSystem';
+import IntelligentChatBot from './IntelligentChatBot';
+import CookieConsent from './CookieConsent';
 
-export default function AppWrapper() {
+const AppWrapper: React.FC = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground">
-      <h1 className="text-4xl font-bold text-center py-20">
-        ZyatrIA Global - Site en construction
-      </h1>
-    </div>
+    <LanguageProvider>
+      <div className="min-h-screen bg-background text-foreground">
+        <NavigationDesignSystem />
+        <main>
+          <HeroDesignSystem />
+          <TrustStatsSimple />
+          <Services />
+          <MicroAgents />
+          <RoadmapDesignSystem />
+          <PricingDesignSystem />
+          <TestimonialsDesignSystem />
+          <FAQDesignSystem />
+          <CTAFinal />
+        </main>
+        <FooterDesignSystem />
+        <IntelligentChatBot />
+        <CookieConsent />
+      </div>
+    </LanguageProvider>
   );
-}
+};
+
+export default AppWrapper;
